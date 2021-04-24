819e596b8a2698aa8d4e5c2174627f318a9bb5f1
Nicholas Ferreira <nichoas_sc@live.com>
2021-04-24 15:03:57 -0300

    router

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Ecommerce from './pages/commerce'
+import NotFound from './pages/notfound';
 
 
 const App = () => (
-  <Ecommerce />
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact={true} component={Ecommerce} />
+      { /*<Route path="/sobre" component={Sobre} />*/}
+      <Route path='*' component={NotFound} />
+    </Switch>
+  </ BrowserRouter>
 );
 
 export default App;
