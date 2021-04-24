c33be9da8a15599ee34f54d0f96a62ff8a6e5393
Nicholas Ferreira <nichoas_sc@live.com>
2021-04-23 23:51:46 -0300

    fix imports

--- src/components/FloatCart/index.js
+++ src/components/FloatCart/index.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
-import { loadCart, removeProduct, changeProductQuantity } from '../../services/cart/actions';
-import { updateCart } from '../../services/total/actions';
+import { loadCart, removeProduct, changeProductQuantity } from '../../redux/cart/actions';
+import { updateCart } from '../../redux/total/actions';
 import CartProduct from './CartProduct';
-import { formatPrice } from '../../services/util';
+import { formatPrice } from '../../redux/util';
 
 import './style.scss';
 
